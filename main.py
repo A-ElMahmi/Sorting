197874a5e1b86f1dd60d87ed110af4d3a4d0d739
Abderrahmane Mahmi <elmahmi.a04@gmail.com>
2020-06-17 15:15:40 +0000

    Added Selection Sort

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import random
+'''
+Python Sorting Algorithms: https://tuxar.uk/popular-sorting-algorithms/
 
-randList = [random.randint(1, 10) for i in range(10)]
+Sorting Algorithms Visualisations: https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
+'''
+
+import random
+randList = [random.randint(1, 10) for i in range(8)]
 print(randList)
 
 
 def bubbleSort(seq):
-  while True:
+  changed = True
+  while changed:
+    changed = False
     for i in range(len(seq)-1):
       if seq[i] > seq[i+1]:
         seq[i], seq[i+1] = seq[i+1], seq[i]
-        continue
-    break
+        changed = True
   return seq
+
+# print(bubbleSort(list(randList)))
 
 
 def insertionSort(seq):
@@ -22,6 +30,27 @@ def insertionSort(seq):
       i -= 1 
   return seq
 
+# print(insertionSort(list(randList)))
 
-print(bubbleSort(randList.copy()))
-print(insertionSort(randList.copy()))
+
+def selectionSort(seq):
+  for x in range(len(seq)):
+    smallest, index = sum(seq), 0
+    for i in range(x, len(seq)):
+      if seq[i] < smallest:
+        smallest, index = seq[i], i
+    seq[x], seq[index] = smallest, seq[x]
+  return seq
+ 
+# print(selectionSort(list(randList)))
+
+
+# def mergeSort(seq):
+#   temp = []
+#   for i in range(len(seq)):
+#     for j in range(0, len(seq)-4):
+#       pass
+    
+#     for k in range(5, len(seq)):
+#       pass
+
